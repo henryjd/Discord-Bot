402a3652e1ffd755edc2e5b3dc46ac01a2906fb0
henryjd <henryjd15@gmail.com>
2019-07-29 13:36:51 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,6 @@
+echo "# Discord-Bot" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/henryjd/Discord-Bot.git
+git push -u origin master
