3124cc490f72a406d4438f3b090562d738aedf43
henryjd <henryjd15@gmail.com>
2019-07-29 13:37:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,29 @@
-echo "# Discord-Bot" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/henryjd/Discord-Bot.git
-git push -u origin master
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
